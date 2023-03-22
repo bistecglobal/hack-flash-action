17a72b7926fd0604b87dce8175780f29f9865418
Chandima Ranaweera <chandima@bistecglobal.com>
2023-03-22 23:37:51 +0530

    add register api end points

---
+++ hackathons/hack-flash/cypress/e2e/api/register.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Register Vehicle Api', () => {
+    beforeEach(() => {
+      // SET Base url from CYPRESS_BASE_URL env variable
+      cy.visit('/')
+    })
+  
+    it('should register new api', () => {
+  
+      cy.request('POST', '/api/vehicle/register', { 
+        vehicleType: "CAR",
+        licencePlate: "ABC-2323"
+       } ).as('registered');
+  
+      cy.get('@types').should((response) => {
+        expect(response.body).to.have.property('registered', true)
+      })
+    })
+  
+  })
+  
