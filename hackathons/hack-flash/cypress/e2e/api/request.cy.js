710d3d938ce3b100c83042cfd3771eacd5e015f3
Chandima Ranaweera <chandima@bistecglobal.com>
2023-03-24 07:40:14 +0530

    add more tests

--- hackathons/hack-flash/cypress/e2e/api/request.cy.js
+++ hackathons/hack-flash/cypress/e2e/api/request.cy.js
@@ -24,18 +24,18 @@ describe('Request Fuel Api', () => {
   it('should request fuel complete successfully within quota', () => {
     cy.registerFromApi('Car', 'kk-2008')
 
-    cy.request('POST', '/api/fuel/request', 
-    JSON.stringify({ "licensePlate": "kk-2008", "amount": 10, "date": "2023-04-28T00:00:00.000Z" }))
-    .as('fuelRequest1');
+    cy.request('POST', '/api/fuel/request',
+      JSON.stringify({ "licensePlate": "kk-2008", "amount": 10, "date": "2023-04-28T00:00:00.000Z" }))
+      .as('fuelRequest1');
 
     cy.get('@fuelRequest1').should((response) => {
       expect(response.body).to.have.property('remaining', 10);
       expect(response.body).to.have.property('used', 10);
     })
 
-    cy.request('POST', '/api/fuel/request', 
-    JSON.stringify({ "licensePlate": "kk-2008", "amount": 10, "date": "2023-04-28T00:00:00.000Z" }))
-    .as('fuelRequest2');
+    cy.request('POST', '/api/fuel/request',
+      JSON.stringify({ "licensePlate": "kk-2008", "amount": 10, "date": "2023-04-28T00:00:00.000Z" }))
+      .as('fuelRequest2');
 
     cy.get('@fuelRequest2').should((response) => {
       expect(response.body).to.have.property('remaining', 0);
@@ -45,9 +45,9 @@ describe('Request Fuel Api', () => {
 
   it('should request fuel fail outside quota', () => {
     cy.registerFromApi('Car', 'kk-2009')
-    cy.request('POST', '/api/fuel/request', 
-    JSON.stringify({ "licensePlate": "kk-2009", "amount": 10, "date": "2023-04-28T00:00:00.000Z" }))
-    .as('fuelRequest1');
+    cy.request('POST', '/api/fuel/request',
+      JSON.stringify({ "licensePlate": "kk-2009", "amount": 10, "date": "2023-04-28T00:00:00.000Z" }))
+      .as('fuelRequest1');
 
     cy.get('@fuelRequest1').should((response) => {
       expect(response.body).to.have.property('remaining', 10);
@@ -58,12 +58,59 @@ describe('Request Fuel Api', () => {
       url: '/api/fuel/request',
       failOnStatusCode: false,
       method: 'POST',
-      body:JSON.stringify({ "licensePlate": "kk-2009", "amount": 20, "date": "2023-04-28T00:00:00.000Z" }) 
+      body: JSON.stringify({ "licensePlate": "kk-2009", "amount": 20, "date": "2023-04-28T00:00:00.000Z" })
     }).as('fuelRequest2')
 
     cy.get('@fuelRequest2').should((response) => {
       expect(response.status).to.equal(500)
     })
   })
+
+  it('should be able to request multiple weeks', () => {
+    cy.registerFromApi('Car', 'kk-2008')
+
+    cy.request('POST', '/api/fuel/request',
+      JSON.stringify({ "licensePlate": "kk-2008", "amount": 20, "date": "2023-04-28T00:00:00.000Z" }))
+      .as('fuelRequest1');
+
+    cy.get('@fuelRequest1').should((response) => {
+      expect(response.body).to.have.property('remaining', 0);
+      expect(response.body).to.have.property('used', 20);
+    })
+
+    cy.request('POST', '/api/fuel/request',
+      JSON.stringify({ "licensePlate": "kk-2008", "amount": 10, "date": "2023-05-28T00:00:00.000Z" }))
+      .as('fuelRequest2');
+
+    cy.get('@fuelRequest2').should((response) => {
+      expect(response.body).to.have.property('remaining', 10);
+      expect(response.body).to.have.property('used', 10);
+    })
+  })
+
+  it('should be able to request multiple vehicles', () => {
+    cy.registerFromApi('Car', 'kk-2008')
+
+    cy.request('POST', '/api/fuel/request',
+      JSON.stringify({ "licensePlate": "kk-2008", "amount": 20, "date": "2023-04-28T00:00:00.000Z" }))
+      .as('fuelRequest1');
+
+    cy.get('@fuelRequest1').should((response) => {
+      expect(response.body).to.have.property('remaining', 0);
+      expect(response.body).to.have.property('used', 20);
+    })
+
+    cy.registerFromApi('Car', 'kk-2009')
+
+    cy.request('POST', '/api/fuel/request',
+      JSON.stringify({ "licensePlate": "kk-2009", "amount": 20, "date": "2023-05-28T00:00:00.000Z" }))
+      .as('fuelRequest2');
+
+    cy.get('@fuelRequest2').should((response) => {
+      expect(response.body).to.have.property('remaining', 0);
+      expect(response.body).to.have.property('used', 20);
+    })
+  })
+
 
 })
